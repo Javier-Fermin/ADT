fe0fabe01343bfda65d35d5428635415fd74476b
Fran de Ysasi <fdeysasigonzalez@gmail.com>
2023-09-20 13:20:06 +0200

    FEAT & UPDATE: First version of the crearUnidadDidactica method inside of the DBImplementation class and URL, USER and PASSWORD parametters added in the properties file.

--- src/dao/DBImplementation.java
+++ src/dao/DBImplementation.java
@@ -5,6 +5,14 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.ConvocatoriaExamen;
 import model.Enunciado;
 import model.UnidadDidactica;
@@ -13,11 +21,46 @@ import model.UnidadDidactica;
  *
  * @author javie
  */
-public class DBImplementation implements Dao{
+public class DBImplementation implements Dao {
 
+    /*    Attributes    */
+
+    private Connection con;
+    private PreparedStatement ptmt;
+    private ResultSet rset;
+
+    /*    Methods    */
+    /**
+     * *
+     * crearUnidadDidactica: Metodo que recoge un objeto UnidadDidactica y lo
+     * guarda en la base de datos
+     *
+     * @param UnidadDidactica ud
+     */
     @Override
     public void crearUnidadDidactica(UnidadDidactica ud) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        con = openConnection();
+
+        try {
+            ptmt = con.prepareStatement("INSERT INTO UnidadDidactica(acronimo,titulo,evaluacion,descripcion_UD) VALUES (?,?,?,?);");
+
+            ptmt.setString(1, ud.getAcronimo());
+            ptmt.setString(2, ud.getTitulo());
+            ptmt.setString(3, ud.getEvaluacion());
+            ptmt.setString(4, ud.getDescripcion());
+
+            ptmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBImplementation.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                ptmt.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBImplementation.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        closeConnection(con);
+
     }
 
     @Override
@@ -44,5 +87,41 @@ public class DBImplementation implements Dao{
     public UnidadDidactica buscarUnidad(Integer id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    /**
+     * *
+     * openConnection method that uses a Connection object to access the
+     * examendb database and creates a connection between the programm and the
+     * database
+     *
+     * @return Connection conn
+     */
+    public Connection openConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(ResourceBundle.getBundle("resources.Properties").getString("URL"), ResourceBundle.getBundle("resources.Properties").getString("USER"), ResourceBundle.getBundle("resources.Properties").getString("PASSWORD"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    /**
+     * *
+     * closeConnection method that recieves a Connection object and uses it to
+     * access the examendb database and close the connection between the program
+     * and the database
+     *
+     * @param conn
+     */
+    public void closeConnection(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
